3f779f358633b78ae24bde0a626a2f49fc18dbfa
Charles Chen <charles.chen@motorolasolutions.com>
2017-11-17 14:14:55 -0800

    Added album art and album title

--- mp3splitter.cpp
+++ mp3splitter.cpp
@@ -1,6 +1,7 @@
 // mp3splitter.cpp : Defines the entry point for the console application.
 //
 
+#include <stdio.h>
 #include <fstream>
 #include <sstream>
 #include <iostream>
@@ -66,18 +67,64 @@ int toSeconds(string ts)
     return sec + min * 60 + hour * 3600;
 }
 
+static std::string& buildcmd(std::string& retVal,
+                const std::string& mp3file,
+                const std::string& title,
+                const std::string& artist, 
+                const std::string& ts, 
+                const std::string& nextTs, 
+                const std::string& albumTitle, 
+                const std::string& albumArt)
+{
+    retVal = "ffmpeg -i " + mp3file;
+    if (albumArt.size() > 0)
+    {
+        retVal += (" -i " + albumArt + " -map 0:0 -map 1:0 -c copy -id3v2_version 3 -metadata:s:v title=\"Album cover\" -metadata:s:v comment=\"Cover (Front)\" ");
+    }
+    else
+    {
+        retVal += " -acodec copy";
+    }
+    retVal += (" -ss "+ts);
+    if (nextTs.size() > 0)
+    {
+        char buffer[10];
+        sprintf(buffer, "%d", toSeconds(nextTs) - toSeconds(ts));
+        retVal += " -t ";
+        retVal += buffer;
+    }
+    retVal += (" -metadata Title=\"" + title + "\" -metadata Artist=\"" + artist +"\"");
+    if (albumTitle.size() > 0)
+    {
+        retVal += (" -metadata Album=\"" + albumTitle + "\"");
+    }
+    retVal += (" \"" + title + ".mp3\"\n");
+    return retVal;
+}
+
 int main(int argc, char**argv)
 {
     if (argc < 4)
     {
-        cerr << "Usage: " << argv[0] << " inputfile.txt input.mp3 \"delimiter\"\n";
+        cerr << "Usage: " << argv[0] << " inputfile.txt input.mp3 \"delimiter\" albumTitle albumArtFile\n";
         exit(-1);
+    }
+    std::string album = "";
+    if (argc > 4)
+    {
+        album = argv[4];
+    }
+    std::string albumArt = "";
+    if (argc > 5)
+    {
+        albumArt = argv[5];
     }
     std::string delimiter = argv[3];
     std::string mp3file = argv[2];
     std::ifstream infile(argv[1]);
     std::string line;
     string artist, title, ts = "";
+    std::string cmd;
     while (std::getline(infile, line))
     {
         vector<string> s = split(line, delimiter);
@@ -85,16 +132,12 @@ int main(int argc, char**argv)
         if (ts.size() > 0)
         {
             string nextTs = s[size-1];
-            cout << "ffmpeg -i " << mp3file << " -acodec copy -ss " << ts
-                << " -t " << toSeconds(nextTs) - toSeconds(ts)
-                << " -metadata Title=\"" << title << "\" -metadata Artist=\"" << artist << "\" \"" << title << ".mp3\"" << endl;
+            cout << buildcmd(cmd, mp3file, title, artist, ts, nextTs, album, albumArt);
         }
         title = s[size-3];
         artist = s[size-2];
         ts = s[size-1];
     }
-    cout << "ffmpeg -i " << mp3file << " -acodec copy -ss " << ts
-        << " -metadata Title=\"" << title << "\" -metadata Artist=\"" << artist << "\" \"" << title << ".mp3\"" << endl;
+    cout << buildcmd(cmd, mp3file, title, artist, ts, "", album, albumArt);
     return 0;
 }
-
